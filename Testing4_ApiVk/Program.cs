5decb686d51748a5d86f57328f2f0ff5bae98650
Denis Ivanov <ltybc3528@mail.ru>
2020-01-09 01:35:00 +0300

    Initial commit

---
+++ Testing4_ApiVk/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Testing4_ApiVk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
